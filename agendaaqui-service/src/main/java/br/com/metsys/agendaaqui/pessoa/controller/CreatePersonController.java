5d1f57fe7d1300462b2b5fb3678fa0db28c0e1ab
Bruno Barros <bruno.barros@zup.com.br>
2019-08-25 02:07:45 -0300

    criando implementação do controller create pessoa

--- agendaaqui-service/src/main/java/br/com/metsys/agendaaqui/pessoa/controller/CreatePersonController.java
+++ agendaaqui-service/src/main/java/br/com/metsys/agendaaqui/pessoa/controller/CreatePersonController.java
@@ -1,4 +1,13 @@
 package br.com.metsys.agendaaqui.pessoa.controller;
 
-public class CreatePersonController {
+import br.com.metsys.agendaaqui.pessoa.CreatePersonApi;
+import br.com.metsys.agendaaqui.pessoa.exception.UseCaseException;
+import br.com.metsys.agendaaqui.pessoa.model.CreatePersonRequest;
+import org.springframework.http.ResponseEntity;
+
+public class CreatePersonController implements CreatePersonApi {
+    @Override
+    public ResponseEntity<Long> execute(CreatePersonRequest createPersonRequest) throws UseCaseException {
+        return null;
+    }
 }
