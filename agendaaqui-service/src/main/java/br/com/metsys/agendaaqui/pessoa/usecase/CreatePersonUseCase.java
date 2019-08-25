bf055848611425475167f049de7d444f349bbeef
Bruno Barros <bruno.barros@zup.com.br>
2019-08-25 00:38:13 -0300

    criando pessoa usecase

--- agendaaqui-service/src/main/java/br/com/metsys/agendaaqui/pessoa/usecase/CreatePersonUseCase.java
+++ agendaaqui-service/src/main/java/br/com/metsys/agendaaqui/pessoa/usecase/CreatePersonUseCase.java
@@ -1,7 +1,31 @@
 package br.com.metsys.agendaaqui.pessoa.usecase;
 
+import br.com.metsys.agendaaqui.pessoa.exception.CreatePersonUseCaseException;
+import br.com.metsys.agendaaqui.pessoa.exception.UseCaseException;
+import br.com.metsys.agendaaqui.pessoa.gateway.SavePersonGateway;
+import br.com.metsys.agendaaqui.pessoa.gateway.database.exception.GatewayException;
+import br.com.metsys.agendaaqui.pessoa.model.PersonDomain;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class CreatePersonUseCase {
+    private SavePersonGateway savePersonGateway;
+
+    @Autowired
+    public CreatePersonUseCase(SavePersonGateway savePersonGateway) {
+        this.savePersonGateway = savePersonGateway;
+    }
+
+    public PersonDomain execute(PersonDomain personDomain) throws UseCaseException {
+        PersonDomain personDomainReturn;
+        try {
+            personDomainReturn = savePersonGateway.execute(personDomain);
+            return personDomainReturn;
+        } catch (GatewayException ex) {
+            throw new CreatePersonUseCaseException("Problema na criação da pessoa", ex);
+        }
+
+    }
 }
+
