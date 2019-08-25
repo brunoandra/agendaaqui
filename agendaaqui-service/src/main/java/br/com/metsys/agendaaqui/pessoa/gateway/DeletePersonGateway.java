10c3406187becd4f4a62afdcf0a763caa2bbf7c2
Bruno Barros <bruno.barros@zup.com.br>
2019-08-25 00:28:21 -0300

    deletando pessoas

--- agendaaqui-service/src/main/java/br/com/metsys/agendaaqui/pessoa/gateway/DeletePersonGateway.java
+++ agendaaqui-service/src/main/java/br/com/metsys/agendaaqui/pessoa/gateway/DeletePersonGateway.java
@@ -1,5 +1,7 @@
 package br.com.metsys.agendaaqui.pessoa.gateway;
 
+import br.com.metsys.agendaaqui.pessoa.gateway.database.exception.GatewayException;
+
 public interface DeletePersonGateway {
-    public void execute(long id);
+    public void execute(long id) throws GatewayException;
 }
--- agendaaqui-service/src/main/java/br/com/metsys/agendaaqui/pessoa/gateway/database/DeletePersonGatewayImp.java
+++ agendaaqui-service/src/main/java/br/com/metsys/agendaaqui/pessoa/gateway/database/DeletePersonGatewayImp.java
@@ -1,6 +1,8 @@
 package br.com.metsys.agendaaqui.pessoa.gateway.database;
 
 import br.com.metsys.agendaaqui.pessoa.gateway.DeletePersonGateway;
+import br.com.metsys.agendaaqui.pessoa.gateway.database.exception.DeleteGatewayException;
+import br.com.metsys.agendaaqui.pessoa.gateway.database.exception.GatewayException;
 import br.com.metsys.agendaaqui.pessoa.gateway.database.repository.PersonRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -15,7 +17,13 @@ public class DeletePersonGatewayImp implements DeletePersonGateway {
     }
 
     @Override
-    public void execute(long id) {
+    public void execute(long id) throws GatewayException {
+        try {
+            personRepository.deleteById(id);
+        }catch (Exception ex)
+        {
+            throw new DeleteGatewayException("Problema ao deletar a pessoa", ex);
+        }
 
     }
 }
