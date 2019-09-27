9791377b90d27256483cb89c0225168103e33fe3
Bruno Barros <bruno.barros@zup.com.br>
2019-09-27 00:23:38 -0300

    incluindo traceid em todos response usando filter

--- agendaaqui-service/src/main/java/br/com/metsys/agendaaqui/pessoa/AgendaAquiApp.java
+++ agendaaqui-service/src/main/java/br/com/metsys/agendaaqui/pessoa/AgendaAquiApp.java
@@ -1,13 +1,19 @@
 package br.com.metsys.agendaaqui.pessoa;
 
+import br.com.metsys.agendaaqui.pessoa.controller.config.TraceHeadersResponse;
 import javafx.application.Application;
 import javafx.stage.Stage;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.filter.GenericFilterBean;
 
 @SpringBootApplication(scanBasePackages = "br.com.metsys.agendaaqui")
 @EnableFeignClients(basePackages = "br.com.metsys.agendaaqui")
+@ServletComponentScan
 public class AgendaAquiApp extends Application {
 
     public static void main(String[] args) {
@@ -17,5 +23,20 @@ public class AgendaAquiApp extends Application {
     @Override
     public void start(Stage primaryStage) {
 
+    }
+
+    @Bean
+    public FilterRegistrationBean someFilterRegistration() {
+
+        FilterRegistrationBean registration = new FilterRegistrationBean();
+        registration.setFilter(myFilter());
+        registration.addUrlPatterns("/*");
+        registration.setOrder(1);
+        return registration;
+    }
+
+    @Bean(name = "someFilter")
+    public GenericFilterBean myFilter() {
+        return new TraceHeadersResponse();
     }
 }
--- agendaaqui-service/src/main/java/br/com/metsys/agendaaqui/pessoa/controller/CreatePersonController.java
+++ agendaaqui-service/src/main/java/br/com/metsys/agendaaqui/pessoa/controller/CreatePersonController.java
@@ -8,7 +8,9 @@ import br.com.metsys.agendaaqui.pessoa.model.PersonDomain;
 import br.com.metsys.agendaaqui.pessoa.usecase.CreatePersonUseCase;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.slf4j.MDC;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -21,19 +23,26 @@ import java.util.Map;
 public class CreatePersonController implements CreatePersonApi {
     private CreatePersonUseCase createPersonUseCase;
     private Logger logger = LoggerFactory.getLogger(FindPersonController.class);
+
     @Autowired
     public CreatePersonController(CreatePersonUseCase createPersonUseCase) {
         this.createPersonUseCase = createPersonUseCase;
     }
 
     @Override
-    public ResponseEntity<Long> execute(@RequestBody final CreatePersonRequest createPersonRequest,@RequestHeader Map<String, String> headers) throws UseCaseException {
+    public ResponseEntity<Long> execute(@RequestBody final CreatePersonRequest createPersonRequest, @RequestHeader Map<String, String> headers) throws UseCaseException {
         headers.forEach((key, value) -> {
             logger.info(String.format("Header '%s' = %s", key, value));
         });
         PersonDomain personDomain = CreatePersonRequestToPersonDomainTranslate.translator(createPersonRequest);
         PersonDomain personDomainReturn = createPersonUseCase.execute(personDomain);
-        ResponseEntity<Long> response = new ResponseEntity<>(personDomainReturn.getId(), HttpStatus.OK);
+
+        HttpHeaders responseHeaders = new HttpHeaders();
+        responseHeaders.set("X-B3-TraceIdMethod",
+                MDC.get("X-B3-TraceId"));
+
+        ResponseEntity<Long> response = new ResponseEntity<>(personDomainReturn.getId(),responseHeaders, HttpStatus.OK);
+
         return response;
     }
 }
